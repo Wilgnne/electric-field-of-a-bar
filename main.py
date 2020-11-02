ab40f4ada34d088ef3364dd7d1327d2e44a76f7d
Wilgnne Khawan <wilgnne.kba@gmail.com>
2020-11-02 16:52:44 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,26 @@
-from electric_field import campo_eletrico_barra
+from electric_field import electric_field_bar
 
+def diff(a, b):
+  return ((b-a)/a)
 init = 1
 end = 3
 
-q = 16*10e-6
+q = 16e-6
 subpartitions = 256
 
-for i in range(1, 257):
-  print("{}: {:e}".format(i, campo_eletrico_barra(init, end, i, q, 0)))
+Q256 = electric_field_bar(init, end, subpartitions, q, 0)
 
+#Campo eletrico por integração da lei de Coulomb
+E = 8.99 * (16/3.0) * 1e3
+#Campo eletrico atual
+x = 1
+#subpartições atuais
+subpartitions = 0
+
+while(diff(x, E) > 1e-2):
+  subpartitions = subpartitions + 1
+  x = electric_field_bar(init, end, subpartitions, q, 0)
+  print("{}\t - {:e}\t == {:e}\t ~= {:.5f}%"
+    .format(subpartitions, x, E, diff(x, E)*100))
+
+print(f"O numero de subpartição para atingir uma precisão mínima de 1% é {subpartitions}.")
